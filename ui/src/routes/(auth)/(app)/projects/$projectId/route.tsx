ea9d237f3d32454ede921b9a70ff18d3d2e29715
linghaoSu <linghao.su@daocloud.io>
2026-03-18 11:20:21 +0800

    fix(ui): rm project switch and use text insteadof slash svg

--- ui/src/routes/(auth)/(app)/projects/$projectId/route.tsx
+++ ui/src/routes/(auth)/(app)/projects/$projectId/route.tsx
@@ -1,54 +1,36 @@
 import {
-  Box, Group, Select, Space, Tabs, Text,
+  Box, Group, Space, Tabs, Text,
 } from '@mantine/core'
 import { Projects } from '@matrixhub/api-ts/v1alpha1/project.pb'
 import {
   createFileRoute,
   notFound,
   Outlet,
+  useLocation,
   useMatchRoute,
   useNavigate,
 } from '@tanstack/react-router'
 import { useTranslation } from 'react-i18next'
 
 import ProjectIcon from '@/assets/svgs/project.svg?react'
-import SlashIcon from '@/assets/svgs/slash.svg?react'
-
-interface SelectOption {
-  value: string
-  label: string
-}
 
 export const Route = createFileRoute('/(auth)/(app)/projects/$projectId')({
   loader: async ({ params: { projectId } }) => {
-    const res = await Projects.ListProjects({
-      page: 1,
-      pageSize: -1,
-    })
-    const projectOptionsMap = new Map<string, SelectOption>()
+    try {
+      const res = await Projects.GetProject({
+        name: projectId,
+      })
 
-    for (const project of res.projects ?? []) {
-      if (!project.name) {
-        continue
+      if (!res) {
+        // TODO: replace this with the dedicated 404 page once it's implemented.
+        throw notFound()
       }
 
-      projectOptionsMap.set(project.name, {
-        value: project.name,
-        label: project.name,
-      })
-    }
-
-    const projectOptions = Array.from(projectOptionsMap.values())
-
-    const hasCurrentProject = projectOptions
-      .some(projectOption => projectOption.value === projectId)
-
-    if (!hasCurrentProject) {
-      // TODO: replace this with the dedicated 404 page once it's implemented.
+      return { project: res }
+    } catch (error) {
+      console.error('Failed to load project data:', error)
       throw notFound()
     }
-
-    return { projectOptions }
   },
   component: RouteComponent,
 })
@@ -74,17 +56,20 @@ const tabRoutes = [
 
 function RouteComponent() {
   const { projectId } = Route.useParams()
-  const { projectOptions } = Route.useLoaderData()
   const { t } = useTranslation()
   const navigate = useNavigate()
   const matchRoute = useMatchRoute()
+  const pathname = useLocation({ select: s => s.pathname })
 
   const activeTabRoute
-    = tabRoutes.find(tab => !!matchRoute({
-      to: tab.to,
-      params: { projectId },
-      fuzzy: true,
-    }))
+    = tabRoutes.find((tab) => {
+      return pathname && !!matchRoute({
+        to: tab.to,
+        params: { projectId },
+        fuzzy: true,
+        pending: true,
+      })
+    })
     ?? tabRoutes[0]
   const activeTabLabel = t(`projects.detail.tabs.${activeTabRoute.value}`)
 
@@ -93,28 +78,13 @@ function RouteComponent() {
       <Space h="lg" />
       <Group gap={4} wrap="nowrap">
         <ProjectIcon width={20} height={20} style={{ color: 'var(--mantine-gray-7)' }} />
-        <Text size="md" c="gray.8">{t('projects.detail.project')}</Text>
-        <Select
-          allowDeselect={false}
-          data={projectOptions}
-          w={160}
-          size="xs"
-          onChange={(value) => {
-            if (!value || value === projectId) {
-              return
-            }
-
-            navigate({
-              to: activeTabRoute.to,
-              params: { projectId: value },
-            })
-          }}
-          value={projectId}
-        />
-
-        <SlashIcon width={24} height={24} style={{ color: 'var(--mantine-color-dimmed)' }} />
-
-        <Text size="md" c="gray.8">{activeTabLabel}</Text>
+        <Text size="md" c="gray.8">
+          {projectId}
+          {' '}
+          /
+          {' '}
+          {activeTabLabel}
+        </Text>
       </Group>
 
       <Tabs
